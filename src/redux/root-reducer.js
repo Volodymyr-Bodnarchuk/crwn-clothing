8a49606a9d313ad0b5ff7260fab8ad0d24fcfdbc
Володимир Боднарчук <you@example.combodnarchuk.volodymyr1@gmail.com>
2021-10-12 12:37:28 +0300

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import shopReducer from "./shop/shop.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
 const persistConfig = {
   key: "root",
@@ -13,5 +16,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
 export default persistReducer(persistConfig, rootReducer);
