56740c02de86f1a9d3e4e6e8128a46445a8ae9a9
Володимир Боднарчук <you@example.combodnarchuk.volodymyr1@gmail.com>
2021-10-23 11:05:39 +0300

    updated our App, sign-in and sign-up components to use hooks

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { connect } from "react-redux";
-import { auth } from "../../firebase/firebase.utils";
 import { createStructuredSelector } from "reselect";
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
